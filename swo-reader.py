5c6cd1ad62d8d2ab1c4b327f0fdbb1a16a7ac0af
Johan Kraft <johan.kraft@percepio.com>
2025-01-13 17:12:11 +0100

    * Did some cleanup, but still doesn't exit properly when closing the IDE. Works on WIn 10 now, was problem with some ports on this machine. Should also work on Linux, but not tested.

--- swo-reader.py
+++ swo-reader.py
@@ -18,7 +18,9 @@ swo_connected = False
 
 global bytecount
 bytecount = 0
-    
+
+global running
+running = True
    
 # TCP client, connects to GDB server and reads SWO data.
 def gdb_swo_reader():
@@ -30,71 +32,77 @@ def gdb_swo_reader():
     global bytecount
     bytecount = 0
     
-    while True:
-        print("[swo-reader] Connecting to GDB server on port " + str(SWO_PORT) + ".")
+    global running
+    
+    
+    print("[swo-reader] Connecting to GDB server on port " + str(SWO_PORT) + ".")
+    
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', SWO_PORT))
+    except:
+        print("[swo-reader] Error connecting, is GDB server started?")
+        running = False
+        return
         
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(('localhost', SWO_PORT))
-        except:
-            print("[swo-reader] Error connecting, is GDB server started?")
-            return
-            
-        print("[swo-reader] Connected.")
-        swo_connected = True
-        
-        file_path = "swo-data.bin"
-        outfile = open(file_path, "wb")
+    print("[swo-reader] Connected.")
+    swo_connected = True
+    
+    file_path = "swo-data.bin"
+    outfile = open(file_path, "wb")
 
-        # This makes the GDB server output the SWO data.        
-        byte_array = bytearray( [1, 0, 0, 0, 0, 0, 0, 0] )
+    # This makes the GDB server output the SWO data.        
+    byte_array = bytearray( [1, 0, 0, 0, 0, 0, 0, 0] )
+    
+    try:
+        s.send(byte_array)
+    except:
+        print("[swo-reader] Error sending to GDB server.")
+        running = False            
+        return
         
-        try:
-            s.send(byte_array)
-        except:
-            print("[swo-reader] Error sending to GDB server.")
-            return
-            
-        bytecount = 0
+    bytecount = 0
+    
+    print("[swo-reader] Ready for data. Writing trace data to: " + file_path)
+    while (swo_connected):
         
-        print("[swo-reader] Ready for data. Writing trace data to: " + file_path)
-        while (swo_connected):
-            
-            try:                              
-                data = s.recv(250*1024)
-                                
-                if (data == b''):
-                    print("[swo-reader] Disconnected.")
-                    swo_connected = False
-                else:
-                    bytecount = bytecount + len(data)
-                    outfile.write(data);
-                
-            except KeyboardInterrupt:
-                print("[swo-reader] Ctrl-C (thread)")
-                swo_connected = False
-
-            except Exception as err:                
+        try:                              
+            data = s.recv(250*1024)
+                            
+            if (data == b''):
                 print("[swo-reader] Disconnected.")
                 swo_connected = False
-                
-        s.close()
-        outfile.close()
-        print("[swo-reader] Reader thread closed.")
+            else:
+                bytecount = bytecount + len(data)
+                outfile.write(data);
+            
+        except KeyboardInterrupt:
+            print("[swo-reader] Ctrl-C (thread)")
+            swo_connected = False
+
+        except Exception as err:                
+            print("[swo-reader] Disconnected.")
+            swo_connected = False
+            
+    s.close()
+    outfile.close()
+    ide_connected = False
+    running = False
+    print("[swo-reader] Reader thread closed.")
 
 
 # STM32CubeIDE feature will connect here, looking for SWO data, but won't get any.
 # Starts the SWO reader when IDE connected...
 
 def ide_swo_fakesender():
-    global ide_connected   
+    global ide_connected
+    global running
     ide_connected = False
-    PORT = 61236  
+    PORT = 61035
     while True:
         ide_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #ide_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        #ide_socket.bind((HOST, PORT))        
-        ide_socket.bind(("127.0.0.1", 61035))
+        ide_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        ide_socket.bind((HOST, PORT))        
         print("[swo-reader] Waiting for IDE connection.")
         ide_socket.listen()
         conn, addr = ide_socket.accept()
@@ -116,7 +124,9 @@ def ide_swo_fakesender():
                     ide_connected = False
             except:
                     ide_connected = False
-                    s.close()
+        
+        running = False
+        s.close()
         print("[swo-reader] IDE disconnected.")
 
 print("[swo-reader] Starting server thread for STM32CubeIDE")
