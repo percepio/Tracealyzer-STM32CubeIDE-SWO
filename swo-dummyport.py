135faba7edd3249dc9e3cf1030850cb67cd10327
Johan Kraft <johan.kraft@percepio.com>
2025-01-28 16:51:19 +0100

    * Started writing "readme" text in the python script. Most should be moved to readme.md.

--- swo-dummyport.py
+++ swo-dummyport.py
@@ -12,13 +12,112 @@ HOST = "127.0.0.1"
 ide_connected = False
 ctrl_c_pressed = False
 
-# The purpose of this script is to trick STM32CubeIDE to start a debugging
-# session with SWO trace output enabled, while the SWO data is read by
-# swo-reader.py instead of the IDE. (The GDB server is configured to send
-# the SWO data to another port, where swo-reader.py connects.)
-# This script provides a "dummy" TCP port for the IDE to read SWO data.
-# No data is provided to the IDE (possible extension) but the connection
-# is successful and we avoid errors in the IDE aborting the debug session.
+# The purpose of this script is to enable Tracealyzer trace streaming 
+# in STM32CubeIDE, using an STLINK v3 connected to the SWO pin, while
+# also allowing for regular debugging in STM32CubeIDE at the same time.
+# 
+# Setup:
+#
+#    1. Configure TraceRecorder to use the ARM_ITM streamport (see ...)
+#
+#    2. Configure SWO output in STM32CubeIDE (ITM port 1 enabled, no timestamps etc.)
+#       -
+#       -
+#
+#    3. Enable SWO output
+#       -
+#       -
+#
+#    4. Update your Debug Configuration in STM32CubeIDE:
+#       -
+#       -
+#
+#    5. Test-run a debug session to check that "swo-data.bin" is created in
+#       your project folder and contains some data.
+#
+#    6. Configure Tracealyzer to read the data from swo-data.bin (PSF streaming settings).
+#       - Open File -> Settings and select "PSF Streaming Settings"
+#       - Target Connection: File System
+#       - File: path/to/swo-data.bin
+#       - Replay mode: Unchecked
+#       - Data is ITM encoded: Checked
+#
+# Usage:
+#
+#    1. Start this script, e.g. by using your "External Tool" shortcut in STM32CubeIDE.
+#       This opens a separate terminal window showing the GDB server log together with
+#       messages from this script.
+#
+#    2. Start your debug session and run your system. The trace data is saved to "swo-data.bin".
+#       
+#       You should see messages labeled "[swo-reader]" about the data rate and total
+#       data received. You may use breakpoints and halt/go debugging without interfering
+#       with the tracing, but the SWV features in STM32CubeIDE will not provide any data.
+#
+#    3. Start a streaming trace session in Tracealyzer, using the setup described above.
+#       This opens the "Live Stream" window. 
+#       Check for a notice about "Missed events" in the bottom-right corner, under "Event Rate".
+#       If this notice shows up, follow the advice under "Troubleshooting". Tracealyzer needs a
+#       complete data stream to correctly display the trace.
+#
+# Troubleshooting:
+# The main risk is "Missed events", i.e. data loss in the transfer. Missed events are detected
+# and reported by Tracealyzer, and typically only occurs if streaming at over 500 KB/s for
+# several minutes when using this approach. In case you see Missed Events, make sure to resolve
+# such issues before studying the traces in Tracealyzer, otherwise the displayed data might be
+# partically incorrect.
+#
+# One reason for Missed events can be the SWO frequency. Up to 7 MHz seems reliable, but in our
+# experiments STM32CubeIDE selected 12 MHz by default (core clock divided by 10) which resulted
+# in unreliable data transmission. It is recommended to select "Limit SWO frequency" and specify
+# a lower value, for example 7 MHz.
+
+# Another  reason for such missed events is most likely the small SWO data buffer in the
+# STLINK GDB server. It seems this can overflow at very high and continuous data rates, probably
+# due to interference from other applications or background activity in the operating system. 
+#
+# If you see Missed events, please try the following:
+#
+#    1. Make sure to limit the SWO frequency to avoid transmission errors.
+#       In your STM32CubeIDE Debug Configuration, open the "Debugger" page and you find
+#       the "Limit SWO clock" in the "Serial View Viewer" panel. Try 6-7 MHz to begin with.
+#       If you still have issues, try a lower value, otherwise you can try a higher value to
+#       see if you can increase the performance a bit.
+#       Note that minor changes in SWO frequency might not have any effect, as the GDB server
+#       has fixed valid baud rates and applies the nearest lower valid setting. This appears to
+#       be in steps of 500 KHz or so. The actual SWO baud rate used can be seen in
+#       the GDB server window. For example, if setting 7000 KHz results in "baudrate 6620000Hz".
+#
+#    2. Disable live visualization in Tracealyzer. This may cause higher load on the
+#       host computer, that may contribute to Missed Events at higher event rates.
+#       - Open Tracealyzer and select "Open Live Stream Tool" from the Trace menu.
+#       - Before starting the session, enable the checkbox "Disable Live Visualization".
+#       - Click "Connect" and "Start Session" to begin reading the data, either while the
+#         target is running or after it has been stopped.
+#       - Select "Stop Session" to show the trace data in Tracealyzer.
+#
+#    3. If this doesn't help, you may try closing other open applications that isn't
+#       needed at the moment, perhaps also Tracealyzer itself (at least if a large
+#       trace is already loaded). Open Tracealyzer again when there is no data transmission,
+#       for example when halted on a breakpoint, and load the data like described above.
+#
+# How it works:
+#
+# The SWO data is provided by the STLINK GDB server on a TCP port, but
+# STM32CubeIDE will normally connect to this port and consume all data.
+#
+# This solution provides a "dummy" TCP port that is used as SWO port
+# in the STM32CubeIDE debug configuration. This script also starts the
+# GDB server via a script. This way, we can configure the GDB server
+# to output the SWO data on a different TCP port number. This script
+# can be configured as an "External Tool" in STM32CubeIDE, so only
+# one extra click is required. 
+#
+# A second python script ("swo-reader.py") connects to this port and saves
+# the SWO data to a file. This is started as a "Run Command" in the debug
+# configuration, which makes it start automatically when the debug session
+# is ready to run.
+#
 
 def sdsdTimestampMillisec64():
     return int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() * 1000) 
